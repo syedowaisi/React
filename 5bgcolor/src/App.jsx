2f5db8cbac199fa70f2632bb396e259efc792b0a
syedowaisi <osyed8452@gmail.com>
2026-02-07 16:30:03 +0530

    bg color change done

---
+++ 5bgcolor/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [color, setColor] = useState('olive')
+
+  return (
+    <div className="w-full h-screen duration-200" style={{backgroundColor: color}}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button onClick={()=> setColor("red")}
+          className='outline-none px-4 py-1' style={{backgroundColor:"red"}}>Red</button>
+        </div>
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button onClick={()=> setColor("blue")}
+          className='outline-none px-4 py-1' style={{backgroundColor:"blue"}}>Blue</button>
+        </div><div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button onClick={()=> setColor("green")}
+          className='outline-none px-4 py-1' style={{backgroundColor:"green"}}>Green</button>
+        </div><div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button onClick={()=> setColor("yellow")}
+          className='outline-none px-4 py-1' style={{backgroundColor:"yellow"}}>Yellow</button>
+        </div><div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button onClick={()=> setColor("purple")}
+          className='outline-none px-4 py-1' style={{backgroundColor:"purple"}}>Purple</button>
+        </div>
+      </div> 
+    </div>
+  )
+}
+
+export default App
